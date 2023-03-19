0218d0dac3e0770ff548f5da438457402c6f7f50
Mohammed.Ibrahim <appsdba.ibrahim@gmail.com>
2023-03-19 11:26:26 +0000

    Lead feature in progress
    * Update lead and get lead functionality done.

--- model/lead.py
+++ model/lead.py
@@ -4,3 +4,11 @@
 # Insert lead query.
 def insert_lead():
   print 'Lead inserted into database.'
+
+# Update lead query.
+def update_lead_query():
+  print 'Lead updated into database.'
+
+# Get lead query.
+def select_lead_query():
+  print 'Lead select from database.'  
