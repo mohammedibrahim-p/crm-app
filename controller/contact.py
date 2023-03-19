754c28a667d229a7a8fe2fd7a3c56df9e426fd21
Mohammed.Ibrahim <appsdba.ibrahim@gmail.com>
2023-03-19 13:13:52 +0000

    Contact feature initialize
    * Wrote create, update functions for contact feature.

---
+++ controller/contact.py
@@ -0,0 +1,18 @@
+# contact controller implementation.
+
+
+# Create contact.
+def create_contact():
+  print 'contact creation done.'
+
+# Update contact.
+def update_contact():
+  print 'contact update done.'
+
+# Get contact.
+def get_contact():
+  print 'Get contact done.'  
+
+# Delete contact.
+def delete_contact():
+  print 'Delete contact done.'  
