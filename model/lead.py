c6a537722bc2b8f4b944de918c59085015c3547b
Mohammed.Ibrahim <appsdba.ibrahim@gmail.com>
2023-03-19 11:34:15 +0000

    Lead feature completed.
    * Delete lead functionality done and tested.

--- model/lead.py
+++ model/lead.py
@@ -11,4 +11,8 @@ def update_lead_query():
 
 # Get lead query.
 def select_lead_query():
-  print 'Lead select from database.'  
+  print 'Lead select from database.'
+
+# Delete lead query.
+def delete_lead_query():
+  print 'Lead deleted from database.'    
